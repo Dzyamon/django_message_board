7bba1bc6dadf7e7b83cb4932f8215b8074148622
Dzyamon <dmitri.olehnovich@gmail.com>
2023-06-16 15:03:42 +0300

    add posts app, 3 posts and template

--- django_message_board/settings.py
+++ django_message_board/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # APPS
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
